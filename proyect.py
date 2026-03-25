b5d1b623b66ddaacdab6312cd868b21a341fbdbe
salmanicole10-collab <salmanicole10@gmail.com>
2026-03-25 13:06:19 -0400

    salma

---
+++ proyect.py
@@ -0,0 +1,17 @@
+import flet as ft
+
+def main(page: ft.Page):
+    page.title = "Sistema de Citas Médicas"
+    page.padding = 20
+
+    # lista para guardar citas (simula base de datos)
+    citas = []
+
+    # mensaje de estado
+    mensaje = ft.Text("")
+
+    # campos de entrada
+    nombre = ft.TextField(label="Nombre del paciente")
+    medico = ft.TextField(label="Médico")
+    fecha = ft.TextField(label="Fecha (dd/mm/aaaa)")
+    hora = ft.TextField(label="Hora (ej: 10:00 AM)")
