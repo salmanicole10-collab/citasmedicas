338113ac8a20ba55e968d57042199cd50f54c06d
salmanicole10-collab <salmanicole10@gmail.com>
2026-04-13 23:41:27 -0400

    medico y estado

--- proyect.py
+++ proyect.py
@@ -16,6 +16,30 @@ def main(page: ft.Page):
     fecha = ft.TextField(label="Fecha (dd/mm/aaaa)")
     hora = ft.TextField(label="Hora (ej: 10:00 AM)")
 
+    tipo_medico = ft.Dropdown(
+        label="Tipo de médico",
+        options=[
+            ft.dropdown.Option("General"),
+            ft.dropdown.Option("Pediatra"),
+            ft.dropdown.Option("Cardiólogo"),
+            ft.dropdown.Option("Dermatólogo"),
+            ft.dropdown.Option("Ginecólogo"),
+            ft.dropdown.Option("Odontólogo")
+        ]
+    )
+
+    estado = ft.Dropdown(
+        label="Estado de la cita",
+        options=[
+            ft.dropdown.Option("Programada"),
+            ft.dropdown.Option("Completada"),
+            ft.dropdown.Option("Cancelada"),
+            ft.dropdown.Option("Perdida")
+        ],
+        value="Programada"
+    )
+
+
     def guardar_cita(e):
         if nombre.value == "" or medico.value == "" or fecha.value == "" or hora.value == "":
             mensaje.value = "Por favor llena todos los campos"
